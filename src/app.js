04c0da0a45cacfe3a3d061eed1343af83af85fc7
Mariano Joaquin Cantos <joaquincantos@hotmail.com>
2023-11-07 20:10:18 -0300

    Agregando index, index.css, css general, headers y footers

--- src/app.js
+++ src/app.js
@@ -8,5 +8,22 @@ app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/login', (req, res) =>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req, res) =>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/productDetail', (req, res) =>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
+
+app.get('/productCart', (req, res) =>{
+    res.sendFile(__dirname + '/views/productCart.html');
+});
+
+
 app.listen(8000, () => 
 console.log("Levantando un servidor con Express"));
