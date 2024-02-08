60d8b80eddd3d40a6e4c399169f8cbf53fa09aae
anavalentinac <camposanavalentina@gmail.com>
2024-02-07 21:46:55 -0300

    corrgiendo diagrama ER y agregando structure.sql

---
+++ structure.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE capucafe_db;
+
+CREATE TABLE `capucafe_db`.`clientes` (
+  `id` INT NOT NULL,
+  `nombre` VARCHAR(50) NOT NULL,
+  `apellido` VARCHAR(50) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(50) NOT NULL,
+  `createdAt` DATETIME NULL,
+  `updatedAt` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC));
+
+CREATE TABLE `capucafe_db`.`categorias` (
+  `id` INT NOT NULL,
+  `categoria` VARCHAR(50) NOT NULL,
+  `createdAt` DATETIME NULL,
+  `updatedAt` DATETIME NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `capucafe_db`.`colores` (
+  `id` INT NOT NULL,
+  `color` VARCHAR(50) NOT NULL,
+  `createdAt` DATETIME NULL,
+  `updatedAt` DATETIME NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `capucafe_db`.`productos` (
+  `id` INT NOT NULL,
+  `nombre` VARCHAR(50) NOT NULL,
+  `descripcion` VARCHAR(255) NULL,
+  `categoriaId` INT NOT NULL,
+  `colorId` INT NULL,
+  `precio` DECIMAL(12) NULL,
+  `createdAt` DATETIME NULL,
+  `updatedAt` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_categoria_producto`
+    FOREIGN KEY (`id`)
+    REFERENCES `capucafe_db`.`categorias` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_color_producto`
+    FOREIGN KEY (`id`)
+    REFERENCES `capucafe_db`.`colores` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE `capucafe_db`.`clientesproductos` (
+  `id` INT NOT NULL,
+  `clienteId` INT NOT NULL,
+  `productoId` INT NOT NULL,
+  `cantidad` INT NOT NULL,
+  `createdAt` DATETIME NULL,
+  `updatedAt` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_clienteid`
+    FOREIGN KEY (`id`)
+    REFERENCES `capucafe_db`.`clientes` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_productoid`
+    FOREIGN KEY (`id`)
+    REFERENCES `capucafe_db`.`productos` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
